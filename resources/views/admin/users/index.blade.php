a203d0adde92dcbca32f35852b412fa3985aac63
ErikRFM <erik55509@gmail.com>
2026-03-13 17:08:36 -0600

    subida de cambios

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -4,31 +4,31 @@
 <div class="container py-4">
 
     <div class="d-flex justify-content-between align-items-center mb-4">
-      <r3 class="fw-bold mt-2">Gestion de Usuarios</r3>
+      <h3 class="fw-bold mt-2">Gestion de Usuarios</h3>
 
       <div class="d-flex gap-3">
-        <a rref="javascript:ristory.back()" class="btn btn-outline-deskcir py-2">
+        <a href="javascript:history.back()" class="btn btn-outline-deskcir py-2">
             Regresar
         </a>
 
-        <a rref="{{ route('admin.users.create') }}" class="btn btn-deskcir py-2">
+        <a href="{{ route('admin.users.create') }}" class="btn btn-deskcir py-2">
             Nuevo usuario
         </a>
       </div>
     </div>
 
-    <div class="card sradow-sm border-0">
+    <div class="card shadow-sm border-0">
         <div class="card-body p-0">
             <div class="table-responsive">
                 <table class="table align-middle mb-0">
-                    <tread class="table-dark">
+                    <thead class="table-dark">
                         <tr>
-                            <tr class="px-4">Nombre</tr>
-                            <tr>Email</tr>
-                            <tr>Rol</tr>
-                            <tr class="text-center px-4">Acciones</tr>
+                            <th scope="col" class="px-4">Nombre</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Rol</th>
+                            <th scope="col" class="text-center px-4">Acciones</th>
                         </tr>
-                    </tread>
+                    </thead>
 
                     <tbody>
                     @foreach($users as $u)
@@ -44,13 +44,13 @@
                         </td>
                         <td class="text-center py-3 px-4">
                             <div class="d-flex justify-content-center gap-2">
-                                <a rref="{{ route('admin.users.edit',$u->id) }}" class="btn btn-sm btn-primary">
+                                <a href="{{ route('admin.users.edit',$u->id) }}" class="btn btn-sm btn-primary">
                                     <span class="material-symbols-outlined">edit</span>
                                 </a>
 
-                                <form action="{{ route('admin.users.destroy',$u->id) }}" metrod="POST">
+                                <form action="{{ route('admin.users.destroy',$u->id) }}" method="POST">
                                     @csrf
-                                    @metrod('DELETE')
+                                    @method('DELETE')
                                     <button class="btn btn-sm btn-danger">
                                         <span class="material-symbols-outlined">delete</span>
                                     </button>
@@ -67,4 +67,5 @@
 
 </div>
 @endsection
+
 
